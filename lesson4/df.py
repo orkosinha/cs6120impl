56b994addd9bcbb3fe5b5d5c524c53ca6b69675d
Orko Sinha <37596785+orkosinha@users.noreply.github.com>
2022-02-20 21:44:30 +0000

    Run black

--- lesson4/df.py
+++ lesson4/df.py
@@ -1,52 +1,61 @@
 COMMUTATIVE = ["add", "sub", "mul", "div", "eq", "and", "or"]
 FOLDABLE = COMMUTATIVE + ["lt", "gt", "le", "ge", "not"]
 fold = {
-    'add' : lambda x, y: x + y,
-    'sub' : lambda x, y: x - y,
-    'mul' : lambda x, y: x * y,
-    'div' : lambda x, y: int(x / y),
-    'eq'  : lambda x, y: x == y,
-    'and' : lambda x, y: x and y,
-    'or'  : lambda x, y: x or y,
-    'lt'  : lambda x, y: x < y,
-    'gt'  : lambda x, y: x > y,
-    'le'  : lambda x, y: x <= y,
-    'ge'  : lambda x, y: x > y,
+    "add": lambda x, y: x + y,
+    "sub": lambda x, y: x - y,
+    "mul": lambda x, y: x * y,
+    "div": lambda x, y: int(x / y),
+    "eq": lambda x, y: x == y,
+    "and": lambda x, y: x and y,
+    "or": lambda x, y: x or y,
+    "lt": lambda x, y: x < y,
+    "gt": lambda x, y: x > y,
+    "le": lambda x, y: x <= y,
+    "ge": lambda x, y: x > y,
 }
+
 
 def cp_transfer(block, in_b):
     out = in_b
     for instr in block:
-        if 'dest' in instr:
-            if instr['op'] == 'const':
-                out[instr['dest']] = instr['value']
-            elif 'args' in instr:
-                if len(instr['args']) == 2 and instr['op'] in FOLDABLE:
-                    argl = instr['args'][0]
-                    argr = instr['args'][1]
+        if "dest" in instr:
+            if instr["op"] == "const":
+                out[instr["dest"]] = instr["value"]
+            elif "args" in instr:
+                if len(instr["args"]) == 2 and instr["op"] in FOLDABLE:
+                    argl = instr["args"][0]
+                    argr = instr["args"][1]
 
-                    if argl in out and out[argl] != '?' and argr in out and out[argr] != '?':
-                        out[instr['dest']] = fold[instr['op']](out[argl], out[argr])
-                elif instr['op'] == 'id':
-                    if instr['args'][0] in out:
-                     out[instr['dest']] = out[instr['args'][0]]
+                    if (
+                        argl in out
+                        and out[argl] != "?"
+                        and argr in out
+                        and out[argr] != "?"
+                    ):
+                        out[instr["dest"]] = fold[instr["op"]](out[argl], out[argr])
+                elif instr["op"] == "id":
+                    if instr["args"][0] in out:
+                        out[instr["dest"]] = out[instr["args"][0]]
                 else:
-                    out[instr['dest']] = '?'
+                    out[instr["dest"]] = "?"
             else:
-                out[instr['dest']] = '?'
+                out[instr["dest"]] = "?"
     return out
-        
+
 
 def cp_merge(preds):
     out = {}
 
     for p in preds:
         for var, val in p.items():
-            if var in out:
-                if out[var] != val:
-                    out[var] = '?'
+            if val == "?":
+                out[var] = "?"
             else:
-                out[var] = val
+                if var in out:
+                    if out[var] != val:
+                        out[var] = "?"
+                else:
+                    out[var] = val
 
     return out
 
@@ -92,7 +101,6 @@ def run_worklist(cfg, name2block):
 def format_vars(var_lst):
     if var_lst:
         return ", ".join(f"{var}: {val}" for var, val in var_lst.items())
-    
 
 
 def print_analysis(name, wl_in, wl_out):
