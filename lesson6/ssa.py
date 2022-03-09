741b421cf7e7d17b956b1390846fdc00f7b5f1f3
Orko Sinha <37596785+orkosinha@users.noreply.github.com>
2022-03-09 04:11:43 +0000

    Fix bugs with to_ssa.bril

--- lesson6/ssa.py
+++ lesson6/ssa.py
@@ -1,5 +1,7 @@
+from ast import Or
 from cfg import CFG
 from dominator import Dominator
+from collections import OrderedDict
 
 
 def rename(block):
@@ -14,6 +16,8 @@ def to_ssa(func):
     defs = {}
     # Map of label to vars that track which vars need phi nodes in a given label for a block
     label2phi = {}
+    # Map of vars to types
+    types = {}
 
     for arg in func.get("args", []):
         defs[arg["name"]] = {cfg.entry}
@@ -25,6 +29,7 @@ def to_ssa(func):
             if "dest" in instr:
                 if instr["dest"] in defs:
                     defs[instr["dest"]].add(label)
+                    types[instr["dest"]] = instr["type"]
                 else:
                     defs[instr["dest"]] = {label}
 
@@ -32,13 +37,8 @@ def to_ssa(func):
     for var, var_defs in defs.items():
         for label in list(var_defs):
             for f in frontier[label]:
-                block = cfg.block(f)
                 if var not in label2phi[f]:
-                    label2phi[label][var] = {
-                        'op': 'phi',
-                        'args': [],
-                        'labels': []
-                    }
+                    label2phi[f][var] = {"op": "phi", "args": [], "labels": []}
                     defs[var].add(f)
 
     stack = {}
@@ -51,45 +51,54 @@ def to_ssa(func):
         stack[arg["name"]] = [arg["name"]]
 
     def fresh(var):
-        new_var = f"{var}" + (f".{counter[var]}" if counter[var] else "")
+        new_var = f"{var}" + (f"_{counter[var]}" if counter[var] else "")
         stack[var].append(new_var)
         counter[var] += 1
         return new_var
-    
-    def rename(label, stack):
-        prev_stack = stack.copy()
+
+    def rename(label):
+        pushes = {}
 
         for var, phi in label2phi[label].items():
-            phi['dest'] = fresh(var)
+            phi["dest"] = fresh(var)
 
         for instr in cfg.block(label):
-            if 'args' in instr:
-                new_args = [stack[arg][-1] for arg in instr['args']]
-                instr['args'] = new_args
-            
-            if 'dest' in instr:
-                instr['dest'] = fresh(instr['dest'])
+            if "args" in instr:
+                new_args = [stack[arg][-1] for arg in instr["args"]]
+                instr["args"] = new_args
+
+            if "dest" in instr:
+                pushes[instr["dest"]] = pushes.get(instr['dest'], 0) + 1
+                instr["dest"] = fresh(instr["dest"])
 
         for succ in cfg.graph[label].successors:
             for var in set(label2phi[succ].keys()):
-                if not stack[var]:
-                    label2phi[succ].pop(var)
+                if stack[var]:
+                    label2phi[succ][var]["args"].append(stack[var][-1])
+                    label2phi[succ][var]["labels"].append(label)
+                    label2phi[succ][var]["type"] = types[var]
                 else:
-                    label2phi[succ][var]['args'].append(stack[var][-1])
-                    label2phi[succ][var]['labels'].append(label)
-        
-        _, imm_dom = d.tree()
+                    label2phi[succ].pop(var)
+                    
+        tree = d.dom_tree()
 
-        for next in imm_dom[label]:
-            rename(next, prev_stack)
-    
-    rename(cfg.entry, stack)
+        for next in tree[label]:
+            rename(next)
+
+        # restore stack
+        for var, count in pushes.items():
+            for i in range(count):
+                stack[var].pop()
+
+
+    rename(cfg.entry)
 
     for label, block in cfg.label2block.items():
         for var, phi in label2phi[label].items():
             block.insert(0, phi)
-    
+
     return cfg.to_bril()
+
 
 def from_ssa(cfg):
     return
